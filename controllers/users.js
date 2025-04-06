a1f7adaf5065a283494d6974c31481f18dbf39de
gerardalessandrov <gerardvigorodrigues@gmail.com>
2025-04-05 19:00:14 -0500

    users.js

--- controllers/users.js
+++ controllers/users.js
@@ -1,195 +1,89 @@
 const mongodb = require("../data/database");
 const ObjectId = require("mongodb").ObjectId;
 
-// Obtener todos los usuarios
 const getAll = async (req, res) => {
-  try {
-    const result = await mongodb
-      .getDatabase()
-      .db()
-      .collection("contacts")
-      .find();
-    result.toArray().then((contacts) => {
-      res.setHeader("Content-Type", "application/json");
-      res.status(200).json(contacts);
-    });
-  } catch (err) {
-    console.error("Error al obtener los usuarios:", err);
-    res.status(500).json({ message: "Error interno del servidor" });
-  }
+  const result = await mongodb.getDatabase().db().collection("contacts").find();
+  result.toArray().then((contacts) => {
+    res.setHeader("Content-Type", "application/json");
+    res.status(200).json(contacts);
+  });
 };
 
-// Obtener un solo usuario por ID
 const getSingle = async (req, res) => {
-  try {
-    const userId = req.params.id;
-
-    // Validación del ID de MongoDB
-    if (!ObjectId.isValid(userId)) {
-      return res.status(400).json({ message: "ID de usuario inválido" });
-    }
-
-    const result = await mongodb
-      .getDatabase()
-      .db()
-      .collection("contacts")
-      .find({ _id: new ObjectId(userId) });
-
-    result.toArray().then((contacts) => {
-      if (contacts.length === 0) {
-        return res.status(404).json({ message: "Usuario no encontrado" });
-      }
-      res.setHeader("Content-Type", "application/json");
-      res.status(200).json(contacts[0]);
-    });
-  } catch (err) {
-    console.error("Error al obtener el usuario:", err);
-    res.status(500).json({ message: "Error interno del servidor" });
-  }
+  const userId = new ObjectId(req.params.id);
+  const result = await mongodb
+    .getDatabase()
+    .db()
+    .collection("contacts")
+    .find({ _id: userId });
+  result.toArray().then((contacts) => {
+    res.setHeader("Content-Type", "application/json");
+    res.status(200).json(contacts[0]);
+  });
 };
-
-// Crear un nuevo usuario
 const createUser = async (req, res) => {
-  const { name, lastname, gmail, phonenumber, birthday, facebook, instagram } =
-    req.body;
-
-  // Validación de campos requeridos
-  if (
-    !name ||
-    !lastname ||
-    !gmail ||
-    !phonenumber ||
-    !birthday ||
-    !facebook ||
-    !instagram
-  ) {
-    return res.status(400).json({ message: "Campos requeridos faltantes." });
-  }
-
-  // Validación de formato de correo electrónico
-  const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
-  if (!emailRegex.test(gmail)) {
-    return res
-      .status(400)
-      .json({ message: "Formato de correo electrónico inválido." });
-  }
-
   const user = {
-    name,
-    lastname,
-    gmail,
-    phonenumber,
-    birthday,
-    facebook,
-    instagram,
+    name: req.body.name,
+    lastname: req.body.lastname,
+    gmail: req.body.gmail,
+    phonenumber: req.body.phonenumber,
+    birthday: req.body.birthday,
+    facebook: req.body.facebook,
+    instagram: req.body.instagram
   };
-
-  try {
-    const response = await mongodb
-      .getDatabase()
-      .db()
-      .collection("contacts")
-      .insertOne(user);
-
-    if (response.acknowledged) {
-      res.status(201).send(); // Usuario creado correctamente
-    } else {
-      res.status(500).json({ message: "Error al crear el usuario" });
-    }
-  } catch (err) {
-    console.error("Error al crear el usuario:", err);
-    res.status(500).json({ message: "Error interno del servidor" });
+  const response = await mongodb
+    .getDatabase()
+    .db()
+    .collection("contacts")
+    .insertOne(user);
+  if (response.acknowledged) {
+    res.status(201).send(); // Usuario creado correctamente
+  } else {
+    res
+      .status(500)
+      .json(response.error || "Some error occurred while creating the user");
   }
 };
 
-// Actualizar un usuario
 const updateUser = async (req, res) => {
-  const userId = req.params.id;
-
-  // Validación del ID de MongoDB
-  if (!ObjectId.isValid(userId)) {
-    return res.status(400).json({ message: "ID de usuario inválido" });
-  }
-
-  const { name, lastname, gmail, phonenumber, birthday, facebook, instagram } =
-    req.body;
-
-  // Validación de campos requeridos
-  if (
-    !name ||
-    !lastname ||
-    !gmail ||
-    !phonenumber ||
-    !birthday ||
-    !facebook ||
-    !instagram
-  ) {
-    return res.status(400).json({ message: "Campos requeridos faltantes." });
-  }
-
-  // Validación de formato de correo electrónico
-  const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
-  if (!emailRegex.test(gmail)) {
-    return res
-      .status(400)
-      .json({ message: "Formato de correo electrónico inválido." });
-  }
-
+  const userId = new ObjectId(req.params.id);
   const user = {
-    name,
-    lastname,
-    gmail,
-    phonenumber,
-    birthday,
-    facebook,
-    instagram,
+    name: req.body.name,
+    lastname: req.body.lastname,
+    gmail: req.body.gmail,
+    phonenumber: req.body.phonenumber,
+    birthday: req.body.birthday,
+    facebook: req.body.facebook,
+    instagram: req.body.instagram,
   };
-
-  try {
-    const response = await mongodb
-      .getDatabase()
-      .db()
-      .collection("contacts")
-      .replaceOne({ _id: new ObjectId(userId) }, user);
-
-    if (response.modifiedCount > 0) {
-      res.status(204).send(); // Usuario actualizado correctamente
-    } else {
-      res.status(404).json({ message: "Usuario no encontrado" });
-    }
-  } catch (err) {
-    console.error("Error al actualizar el usuario:", err);
-    res.status(500).json({ message: "Error interno del servidor" });
+  const response = await mongodb
+    .getDatabase()
+    .db()
+    .collection("contacts")
+    .replaceOne({ _id: userId }, user);
+  if (response.modifiedCount > 0) {
+    res.status(204).send();
+  } else {
+    res
+      .status(500)
+      .json(response.error || "Some error ocurred while updating the user");
   }
 };
-
-// Eliminar un usuario
 const deleteUser = async (req, res) => {
-  const userId = req.params.id;
-
-  // Validación del ID de MongoDB
-  if (!ObjectId.isValid(userId)) {
-    return res.status(400).json({ message: "ID de usuario inválido" });
-  }
-
-  try {
-    const response = await mongodb
-      .getDatabase()
-      .db()
-      .collection("contacts")
-      .deleteOne({ _id: new ObjectId(userId) });
-
-    if (response.deletedCount > 0) {
-      res.status(204).send(); // Usuario eliminado correctamente
-    } else {
-      res.status(404).json({ message: "Usuario no encontrado" });
-    }
-  } catch (err) {
-    console.error("Error al eliminar el usuario:", err);
-    res.status(500).json({ message: "Error interno del servidor" });
+  const userId = new ObjectId(req.params.id);
+  const response = await mongodb
+    .getDatabase()
+    .db()
+    .collection("contacts")
+    .deleteOne({ _id: userId });
+  if (response.deletedCount > 0) {
+    res.status(204).send();
+  } else {
+    res
+      .status(500)
+      .json(response.error || "Some error ocurred while updating the user");
   }
 };
-
 module.exports = {
   getAll,
   getSingle,
